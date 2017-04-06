ba8102770a94e250941ee47c23199f59a854a7ff
Jorge <jroldanreal@gmail.com>
2017-04-06 23:07:51 +0200

    Initial Commit

---
+++ app/src/main/java/ejercicio31/a31_guardar_localizaciones/MainActivity.java
@@ -0,0 +1,13 @@
+package ejercicio31.a31_guardar_localizaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
